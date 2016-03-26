c7c8d4d7cee1b2e652661be1a5c6e60dd07fef6f
iindigoo <indigobaloe@gmail.com>
2016-03-26 19:49:20 +0100

    1

--- templates/header.php
+++ templates/header.php
@@ -7,32 +7,33 @@
 	<link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-<?
-if ($_SERVER['REQUEST_URI'] == '/rustserver/store.php') {
-  echo "halo";
-
-}
-?>
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
+<header>
+<nav class="navbar">
+  <div class="container container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapser" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="navbar-collapser">
-      <ul class="nav navbar-nav">
-        <li class=""><a href="index.php">Home</a></li>
-        <li><a href="store.php">Store</a></li>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="navbar-left navbar-nav">
+        <li><a href="">Home</a></li>
+      </ul>
+      <ul class="navbar-right navbar-nav">
+        <li><a href="#">Server info</a></li>
+        <li><a href="#">Donations</a></li>
+        <li><a href="#">Vote</a></li>
+        <li><a href="#">Contact</a></li>
       </ul>
     </div>
   </div>
 </nav>
+<h1>Best Server</h1>
+<p>World's best rust server</p>
+</header>
